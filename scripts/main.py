544089205361626f487610f291284cf4ad9d4368
Abby Fry <abbymfry@gmail.com>
2020-12-02 13:32:36 -0500

    saving openCV frame

--- scripts/main.py
+++ scripts/main.py
@@ -7,8 +7,8 @@ import rospy
 from sensor_msgs.msg import Image
 
 class ImageExtractor():
-    """ This ImageExtractor class converts a ROS video stream 
-        to OpenCV format, and then identifies images that can 
+    """ This ImageExtractor class converts a ROS video stream
+        to OpenCV format, and then identifies images that can
         be fed into an image classifier. """
 
     def __init__(self, image_topic):
@@ -16,6 +16,11 @@ class ImageExtractor():
 
         self.cv_image = None        # the latest image from the camera
         self.bridge = CvBridge()    # used to convert ROS messages to OpenCV
+
+        self.x = None
+        self.y = None
+        self.w = None
+        self.h = None
 
         rospy.Subscriber(image_topic, Image, self.get_image)
 
@@ -35,33 +40,28 @@ class ImageExtractor():
         blurred_frame = cv2.GaussianBlur(img, (5, 5), 0)
         gray = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2GRAY)
 
-        #_, threshold = cv2.threshold(frame, 150, 255, cv2.THRESH_BINARY)
         thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
                                             cv2.THRESH_BINARY, 73, 5)
 
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        #cv2.drawContours(img_contours, contours, -1, (0,255,0), 3)
+        cv2.drawContours(img_contours, contours, -1, (0,255,0), 3)
+        areas = [cv2.contourArea(c) for c in contours]
+        max_index = np.argmax(areas)
+        cnt=contours[max_index]
+        approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
+        self.x,self.y,self.w,self.h = cv2.boundingRect(cnt)
+        rect = cv2.rectangle(img,(self.x,self.y),(self.x+self.w,self.y+self.h),(0,255,0),3)
+        #cv2.drawContours(img, len(approx), (0,55,55), 5)
+        cv2.putText(img, "Countour", (self.x,self.y), font, 1, color)
 
-        for cnt in contours:
-            approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
-            x = approx.ravel()[0]
-            y = approx.ravel()[1]
-            area = cv2.contourArea(cnt)
-            if 1000 < area < 100000:
-                cv2.drawContours(img, [approx], 0, (0,55,55), 5)
-                if len(approx) == 3:
-                    cv2.putText(img, "Triangle", (x, y), font, 1, color)
-                elif len(approx) == 4:
-                    cv2.putText(img, "Rectangle", (x, y), font, 1, color)
-                elif len(approx) == 5:
-                    cv2.putText(img, "Pentagon", (x, y), font, 1, color)
-                elif len(approx) == 6:
-                    cv2.putText(img, "Hexagon", (x, y), font, 1, color)
-                elif  15< len(approx) < 50:
-                    cv2.putText(img, "Circle", (x, y), font, 1, color)
-
-        cv2.imshow("shapes", img)
+        cv2.imshow("shapes", img_contours)
+        cv2.imshow("rect", rect)
         cv2.imshow("Threshold", thresh)
+
+    def save_image(self, img):
+        roi = img[self.y:self.y+self.h, self.x:self.x+self.w]
+        print(type(roi))
+        cv2.imwrite("roi.png", roi)
 
     def run(self):
         r = rospy.Rate(5)
@@ -70,6 +70,7 @@ class ImageExtractor():
                 self.sign_localizer(self.cv_image)
                 print(self.cv_image.shape)
                 cv2.imshow('binary_window', self.cv_image)
+                self.save_image(self.cv_image)
                 cv2.waitKey(5)
 
             # start out not issuing any motor commands
@@ -89,9 +90,8 @@ if __name__ == '__main__':
     # i.e. CNN should be pre-trained, and we want to use it!
     # Save gazebo world as .launch file
     # Make one more sign model (new texture)
-    # Need to hone video shape/sign detection situation for Gazebo world 
+    # Need to hone video shape/sign detection situation for Gazebo world
     # i.e. maybe play with threshold
     # MVP: try to get sign recognition working for basic gazebo world by next Wednesday,
     # and plan to improve it by Dec 13. Then finish documenting!
     # Talk about our 3 classes - think about architecture
-
