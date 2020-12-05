7ec480f658a2d339b1f3bcb1e6ed48676b79015e
amfry <abbymfry@gmail.com>
2020-12-05 15:28:23 -0500

    traing CNN & loading data

--- scripts/main.py
+++ scripts/main.py
@@ -6,6 +6,12 @@ from cv_bridge import CvBridge
 import rospy
 from sensor_msgs.msg import Image
 from sign_classification import ConvNeuralNet
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
 
 class ImageExtractor():
     """ This ImageExtractor class converts a ROS video stream
@@ -62,25 +68,101 @@ class ImageExtractor():
         cv2.imwrite("roi.png", self.roi_image)
 
 class SignRecognition():
-    def __init__(self):
+    def __init__(self, downloaded_data_path, image_path):
         self.img_extractor = ImageExtractor("/camera/image_raw")
         self.cnn = ConvNeuralNet()
-        self.downloaded_data_path = '/home/vscheyer/Desktop/traffic_sign_dataset/archive/'
-        self.image_path = '/home/vscheyer/catkin_ws/src/computer_vision/scripts/images'
+        self.downloaded_data_path = downloaded_data_path
+        self.image_path = image_path
         self.selected_categories = [range(1,57)]
+        self.epochs = 25
+        self.model = None
+        self.history = None
+        self.train_ds = None
+        self.N_CLASSES = len(self.selected_categories)
+        self.IMG_HEIGHT = 64
+        self.IMG_WIDTH = 64
+        self.CHANNELS = 3
+        self.val_ds = None
+        self.batch_size = 32
 
-    # def classify_signs(self):
+    def load_data(self, image_dir, categories,
+                  img_h, img_w, batch, grayscale):
+
+        self.N_CLASSES = len(categories) # CHANGE HERE, total number of classes
+        self.IMG_HEIGHT = img_h # CHANGE HERE, the image height to be resized to
+        self.IMG_WIDTH = img_w # CHANGE HERE, the image width to be resized to
+        if (grayscale == 0):
+            self.CHANNELS = 3 # The 3 color channels, change to 1 if grayscale
+        else:
+            self.CHANNELS = 1
+        self.batch_size = batch
+
+        self.train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        image_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(self.IMG_HEIGHT, self.IMG_WIDTH),
+        batch_size=self.batch_size)
+
+        self.val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        image_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(self.IMG_HEIGHT, self.IMG_WIDTH),
+        batch_size=self.batch_size)
+
+        class_names = self.train_ds.class_names
+
+        AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+        self.train_ds = self.train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE) #keep images in memory
+        self.val_ds = self.val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+        normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+
+    def train_cnn(self):
+        self.model = Sequential([
+        layers.experimental.preprocessing.Rescaling(1./255, input_shape=(self.IMG_HEIGHT, self.IMG_WIDTH, 3)),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        # layers.Conv2D(32, 3, padding='same', activation='relu'),
+        # layers.MaxPooling2D(),
+        # layers.Conv2D(64, 3, padding='same', activation='relu'),
+        # layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(self.N_CLASSES)
+        ])
+
+        self.model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+
+        self.model.summary()
+
+        # self.history = self.model.fit(
+        # self.train_ds,
+        # validation_data=self.val_ds,
+        # epochs=self.epochs
+        # )
+
+    def detect_image();
+        pass
 
     def run(self):
         r = rospy.Rate(5)
+        self.load_data(self.image_path, self.selected_categories, 64, 64, 32, 0)
+        self.train_cnn()
         while not rospy.is_shutdown():
-            if not self.cv_image is None:
-                self.sign_localizer(self.img_extractor.cv_image)
+            if not self.img_extractor.cv_image is None:
+                self.img_extractor.sign_localizer(self.img_extractor.cv_image)
                 #visualize video feedq
                 cv2.imshow('binary_window', self.img_extractor.cv_image)
                 cv2.imshow('Threshold', self.img_extractor.threshold_image)
                 cv2.imshow('Contours', self.img_extractor.contour_image)
-                self.img_extractorsave_image(self.cv_image)
+                self.img_extractor.save_image(self.img_extractor.cv_image)
                 cv2.waitKey(5)
             r.sleep()
             if cv2.waitKey(1) & 0xFF == ord('q'): #kill open CV windows
@@ -89,8 +171,11 @@ class SignRecognition():
 
 
 if __name__ == '__main__':
-    sign_recognition = SignRecognition()
+    sign_recognition = SignRecognition("/home/abbymfry/Desktop/chinese_traffic_signs/", "/home/abbymfry/catkin_ws/src/computer_vision/scripts/images")
     sign_recognition.run()
+
+
+    ### next step is to predict with ROI using model.predict, need to make sure it is right pixel sizing
 
     # TODO
     # Figure out sign detected --> sign identified pipeline
